1aa671f69855879c25a79dea8348cc335cf7cca2
RahulSonpaliya <rahulsonpaliya@gmail.com>
2025-01-10 16:12:46 +0530

    added some todo items in main-viewmodel

--- apps/food_express_customer/lib/views/main/main_viewmodel.dart
+++ apps/food_express_customer/lib/views/main/main_viewmodel.dart
@@ -3,11 +3,20 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 import '../../app/locator.dart';
+import '../../data/model/bean/user.dart';
 
 class MainViewModel extends BaseViewModel {
   final _navigationService = locator.get<NavigationService>();
   int _tabIndex = 0;
   int get tabIndex => _tabIndex;
+
+  MainViewModel() {
+    if (appUser.value != null) {
+      _getCartData();
+    }
+    // TODO: implement
+    // _handleNotifications();
+  }
 
   updateTabIndex(i) {
     _tabIndex = i;
@@ -17,5 +26,9 @@ class MainViewModel extends BaseViewModel {
   navigateToLogin() {
     _navigationService.navigateWithTransition(LoginView(fromInside: true),
         transitionStyle: Transition.rightToLeft);
+  }
+
+  _getCartData() async {
+    // TODO: implement
   }
 }
