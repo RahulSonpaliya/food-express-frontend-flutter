5d90351820741ea157e0c7a993c46d2422fba749
RahulSonpaliya <rahulsonpaliya@gmail.com>
2025-08-27 10:46:47 +0530

    implemented markets by category api

--- apps/food_express_customer/lib/views/main/home/category_detail_view/category_detail_viewmodel.dart
+++ apps/food_express_customer/lib/views/main/home/category_detail_view/category_detail_viewmodel.dart
@@ -1,16 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:shared/common_utils.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 import '../../../../app/locator.dart';
 import '../../../../data/model/bean/category.dart';
 import '../../../../data/model/bean/market.dart';
+import '../../../../data/model/bean/user_address.dart';
+import '../../../../data/remote/repository.dart';
 
 class CategoryDetailViewModel extends BaseViewModel {
   List<Category> categoryList;
   Category category;
-  // TODO implement
-  // AllMarketsResponse _allMarketsResponse;
-  List<Market> marketList = List.empty(growable: true);
+  List<Market> _marketList = List.empty(growable: true);
+  List<Market> get marketList => _marketList;
   final _navigationService = locator.get<NavigationService>();
 
   CategoryDetailViewModel(this.category, this.categoryList) {
@@ -18,28 +21,24 @@ class CategoryDetailViewModel extends BaseViewModel {
   }
 
   _getMarketsByCategory() async {
-    // TODO implement
-    // UserAddress userAddress = await UserAddress.getSavedUserAddress();
-    // var result = await locator<Repository>().getMarketByCategory(
-    //     category.id, userAddress.latitude, userAddress.longitude,
-    //     page: currentPage);
-    // if (!loadMore) {
-    //   setBusyForObject(marketList, false);
-    // } else {
-    //   _updateLoadMoreFlag(false);
-    // }
-    // result.fold(
-    //   (failure) => handleFailure(failure),
-    //   (response) {
-    //     _allMarketsResponse = response;
-    //     if (!loadMore) {
-    //       marketList = response.markets;
-    //     } else {
-    //       marketList.addAll(response.markets);
-    //     }
-    //     rebuildUi();
-    //   },
-    // );
+    UserAddress userAddress = await UserAddress.getSavedUserAddress();
+    setBusyForObject(_marketList, true);
+    var result = await locator<Repository>().getNearbyMarkets(requestBody: {
+      "latitude": userAddress.latitude,
+      "longitude": userAddress.longitude,
+      "categoryId": category.id.toString(),
+    });
+    setBusyForObject(_marketList, false);
+    result.fold((failure) {
+      debugPrint('failure $failure');
+      showRetryDialog(failure: failure);
+    }, (marketsResponse) async {
+      if (marketsResponse.success) {
+        _marketList = marketsResponse.marketList;
+        notifyListeners();
+        debugPrint('response${marketsResponse.marketList.length}');
+      }
+    });
   }
 
   loadMoreMarkets() {
